51bbd8d3af8301af4c63301a3a8034049da99f1b
andreidae <60341631+andreidae@users.noreply.github.com>
2020-05-21 09:32:22 +0200

    Update SatsumaSynteny.sh

--- SatsumaSynteny.sh
+++ SatsumaSynteny.sh
@@ -5,4 +5,4 @@
 #3 - Target sequence (Chromosomes)
 #4 - Output_directory
 
-~/Software/satsuma/SatsumaSynteny -m 1 -n $1 -q $2 -t $3 -o $4
+/groups/hologenomics/andreaac/Software/satsuma/SatsumaSynteny -m 1 -n $1 -q $2 -t $3 -o $4
